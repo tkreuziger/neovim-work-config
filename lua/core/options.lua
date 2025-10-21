95709162d5c629877943e339a0491e71e3f8db4a
Tristan Kreuziger <tristan.kreuziger.extern@atruvia.de>
2025-10-21 12:10:19 +0200

    Reordered some options

--- lua/core/options.lua
+++ lua/core/options.lua
@@ -24,6 +24,7 @@ vim.o.scrolloff = 8
 
 -- Show colored column at 80.
 vim.o.colorcolumn = '80'
+vim.o.ruler = true
 
 -- Enable clipboard integration.
 vim.o.clipboard = 'unnamedplus'
@@ -120,33 +121,44 @@ vim.diagnostic.config({
         },
     },
 })
-vim.opt.cmdheight = 0                           -- more space in the neovim command line for displaying messages
-vim.opt.conceallevel = 0                        -- so that `` is visible in markdown files
-vim.opt.fileencoding = "utf-8"                  -- the encoding written to a file
-vim.opt.pumheight = 10                          -- pop up menu height
-vim.opt.showtabline = 2                         -- always show tabs
-vim.opt.smartindent = true                      -- make indenting smarter again
-vim.opt.swapfile = false                        -- creates a swapfile
-vim.opt.termguicolors = true                    -- set term gui colors (most terminals support this)
-vim.opt.timeoutlen = 1000                       -- time to wait for a mapped sequence to complete (in milliseconds)
-vim.opt.undofile = true                         -- enable persistent undo
-vim.opt.updatetime = 300                        -- faster completion (4000ms default)
-vim.opt.writebackup = false                     -- if a file is being edited by another program (or was written to file while editing with another program), it is not allowed to be edited
-vim.opt.laststatus = 3                          -- only the last window will always have a status line
-vim.opt.showcmd = false                         -- hide (partial) command in the last line of the screen (for performance)
-vim.opt.ruler = true                            -- hide the line and column number of the cursor position
-vim.opt.numberwidth = 2                         -- minimal number of columns to use for the line number {default 4}
-vim.opt.wrap = false                            -- display lines as one long line
-vim.opt.sidescrolloff = 8                       -- minimal number of screen columns to keep to the left and right of the cursor if wrap is `false`
-vim.opt.fillchars.eob=" "                       -- show empty lines at the end of a buffer as ` ` {default `~`}
-vim.opt.shortmess:append "c"                    -- hide all the completion messages, e.g. "-- XXX completion (YYY)", "match 1 of 2", "The only match", "Pattern not found"
-vim.opt.whichwrap:append("<,>,[,],h,l")         -- keys allowed to move to the previous/next line when the beginning/end of line is reached
-vim.opt.iskeyword:append("-")                   -- treats words with `-` as single words
-vim.opt.formatoptions:remove({ "c", "r", "o" }) -- This is a sequence of letters which describes how automatic formatting is to be done
-vim.opt.linebreak = true
 
+-- No swwap file.
+vim.o.swapfile = false
+
+-- Enable persistent undo.
+vim.o.undofile = true
+
+-- Interface.
+vim.o.pumheight = 10                          -- pop up menu height
+vim.o.showtabline = 2                         -- always show tabs
+vim.o.cmdheight = 0                           -- more space in the neovim command line for displaying messages
+vim.o.laststatus = 3                          -- only the last window will always have a status line
+vim.o.wrap = false                            -- display lines as one long line
+vim.o.numberwidth = 2                         -- minimal number of columns to use for the line number {default 4}
+vim.o.linebreak = true
+vim.o.showcmd = false                         -- hide (partial) command in the last line of the screen (for performance)
+vim.o.conceallevel = 0                        -- so that `` is visible in markdown files
+vim.o.sidescrolloff = 8                       -- minimal number of screen columns to keep to the left and right of the cursor if wrap is `false`
+vim.o.termguicolors = true                    -- set term gui colors (most terminals support this)
+vim.o.fillchars.eob=" "                       -- show empty lines at the end of a buffer as ` ` {default `~`}
+vim.o.shortmess:append "c"                    -- hide all the completion messages, e.g. "-- XXX completion (YYY)", "match 1 of 2", "The only match", "Pattern not found"
+vim.o.whichwrap:append("<,>,[,],h,l")         -- keys allowed to move to the previous/next line when the beginning/end of line is reached
+vim.o.iskeyword:append("-")                   -- treats words with `-` as single words
+vim.o.formatoptions:remove({ "c", "r", "o" }) -- This is a sequence of letters which describes how automatic formatting is to be done
+
+-- Timing.
+vim.o.timeoutlen = 1000                       -- time to wait for a mapped sequence to complete (in milliseconds)
+vim.o.updatetime = 300                        -- faster completion (4000ms default)
+
+-- Text and code.
+vim.o.fileencoding = "utf-8"                  -- the encoding written to a file
+vim.o.smartindent = true                      -- make indenting smarter again
+vim.cmd('syntax on')
+
+-- Disable deprecate warnings.
 vim.deprecate = function() end
 
+-- Font settings and utilities.
 vim.g.gui_font_default_size = 12
 vim.g.gui_font_size = vim.g.gui_font_default_size
 vim.g.gui_font_face = "Hack Nerd Font Propo"
