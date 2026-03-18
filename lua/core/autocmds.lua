ee094ed5f78242fb311dc010db8965a0f3fb4cd0
Tristan Kreuziger <tristan.kreuziger.extern@atruvia.de>
2026-03-18 14:18:50 +0100

    Reenabled ts folding.

--- lua/core/autocmds.lua
+++ lua/core/autocmds.lua
@@ -73,15 +73,16 @@ autocmd({ 'BufWritePre' }, { pattern = { '*' }, command = [[%s/\s\+$//e]] })
 
 autocmd({ 'FileType' }, {
     callback = function()
---         if require('nvim-treesitter.parsers').has_parser() then
---             vim.opt.foldmethod = 'expr'
---             vim.opt.foldexpr = 'nvim_treesitter#foldexpr()'
---         else
---             vim.opt.foldmethod = 'syntax'
---         end
+        if require('nvim-treesitter.parsers').has_parser() then
+            vim.opt.foldmethod = 'expr'
+            vim.opt.foldexpr = 'nvim_treesitter#foldexpr()'
+        else
+            vim.opt.foldmethod = 'syntax'
+        end
     end,
 })
 
+-- LSP loading spinner.
 local progress = vim.defaulttable()
 vim.api.nvim_create_autocmd("LspProgress", {
   callback = function(ev)
