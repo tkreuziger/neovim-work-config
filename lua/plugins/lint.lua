3ecf61a78fc5d2784d34fd2da89b1685b2ca8e3b
Tristan Kreuziger <tristan.kreuziger.extern@atruvia.de>
2026-03-18 14:18:39 +0100

    Formatting.

--- lua/plugins/lint.lua
+++ lua/plugins/lint.lua
@@ -1,55 +1,22 @@
-vim.g.linting_enabled = true
-
 return {
-    'mfussenegger/nvim-lint',
-    event = { 'BufReadPre', 'BufNewFile' },
-    opts = {},
-    config = function()
-        local lint = require('lint')
-        lint.linters_by_ft = {
-            -- lua = { 'selene' },
-            python = { 'mypy' },
-            dockerfile = { 'hadolint' },
-            json = { 'jsonlint' },
-            yaml = { 'yamllint' },
-        }
+	"mfussenegger/nvim-lint",
+	event = { "BufReadPre", "BufNewFile" },
+	opts = {},
+	config = function()
+		local lint = require("lint")
+		lint.linters_by_ft = {
+			lua = { 'selene' },
+			python = { "mypy" },
+			dockerfile = { "hadolint" },
+			json = { "jsonlint" },
+			yaml = { "yamllint" },
+		}
 
-        local lint_augroup =
-            vim.api.nvim_create_augroup('lint', { clear = true })
-
-        vim.api.nvim_create_autocmd(
-            { 'BufEnter', 'BufWritePost', 'InsertLeave' },
-            {
-                group = lint_augroup,
-                callback = function()
-                    if vim.g.linting_enabled then
-                        lint.try_lint()
-                    end
-                end,
-            }
-        )
-
-        local toggle_lint = function()
-            vim.g.linting_enabled = not vim.g.linting_enabled
-
-            if not vim.g.linting_enabled then
-                vim.diagnostic.reset(nil, 0)
-            else
-                lint.try_lint()
-            end
-        end
-
-        vim.keymap.set('n', '<leader>lll', function()
-            lint.try_lint()
-        end, { desc = 'Lint buffer' })
-        vim.keymap.set('n', '<leader>llc', function()
-            vim.diagnostic.reset(nil, 0)
-        end, { desc = 'Clear linter messages' })
-        vim.keymap.set(
-            'n',
-            '<leader>llt',
-            toggle_lint,
-            { desc = 'Toggle linting' }
-        )
-    end,
+		vim.keymap.set("n", "<leader>lll", function()
+			lint.try_lint()
+		end, { desc = "Lint buffer" })
+		vim.keymap.set("n", "<leader>llc", function()
+			vim.diagnostic.reset(nil, 0)
+		end, { desc = "Clear linter messages" })
+	end,
 }
--- lua/plugins/miniicons.lua
+++ lua/plugins/miniicons.lua
@@ -1,1 +1,1 @@
-return { 'nvim-mini/mini.icons', version = '*' }
+return { "nvim-mini/mini.icons", version = "*" }
