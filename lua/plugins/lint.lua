dbef7517e1e71dd7e399ab4471fd8b1aff66f82a
Tristan Kreuziger <tristan.kreuziger.extern@atruvia.de>
2026-03-18 12:50:52 +0100

    Disabled linter.

--- lua/plugins/lint.lua
+++ lua/plugins/lint.lua
@@ -7,7 +7,7 @@ return {
     config = function()
         local lint = require('lint')
         lint.linters_by_ft = {
-            lua = { 'selene' },
+            -- lua = { 'selene' },
             python = { 'mypy' },
             dockerfile = { 'hadolint' },
             json = { 'jsonlint' },
