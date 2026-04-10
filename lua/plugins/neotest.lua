ac0e6c8653f76120b7d8333fdf307ea39095ff34
Tristan Kreuziger <tristan.kreuziger.extern@atruvia.de>
2026-04-10 09:48:02 +0200

    Added plugins.

---
+++ lua/plugins/neotest.lua
@@ -0,0 +1,19 @@
+return {
+	"nvim-neotest/neotest",
+	dependencies = {
+		"nvim-neotest/nvim-nio",
+		"nvim-lua/plenary.nvim",
+		"antoinemadec/FixCursorHold.nvim",
+		"nvim-treesitter/nvim-treesitter",
+		"nvim-neotest/neotest-python",
+	},
+	config = function(_, opts)
+		require("neotest").setup({
+			adapters = {
+				require("neotest-python")({
+					dap = {},
+				}),
+			},
+		})
+	end,
+}
